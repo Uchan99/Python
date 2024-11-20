c1e464c7a027004bcbff1fecdf2e7544e15ae016
Hur Youchan <126740133+graphicst@users.noreply.github.com>
2024-11-21 00:36:56 +0900

    Title: 남녀노소 구분짓기, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241121/남녀노소 구분짓기/sex-and-age.py
+++ 241121/남녀노소 구분짓기/sex-and-age.py
@@ -2,12 +2,12 @@ s = int(input())
 a = int(input())
 
 if s == 0:
-    if a < 20:
+    if a < 19:
         print('BOY')
     else:
         print('MAN')
 else:
-    if a < 20:
+    if a < 19:
         print('GIRL')
     else:
         print('WOMAN')
