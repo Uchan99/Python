9f5b5ca047d228cb27d51a6a812464bf4a8223cb
Hur Youchan <126740133+graphicst@users.noreply.github.com>
2024-11-27 00:26:36 +0900

    Title: 홀수만 더하기, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241127/홀수만 더하기/add-only-odd-numbers.py
+++ 241127/홀수만 더하기/add-only-odd-numbers.py
@@ -5,7 +5,7 @@ sum_val = 0
 
 for i in range(n):
     arr.append(int(input()))
-    if i % 2 == 1 and i % 3 ==0:
+    if arr[i] % 2 == 1 and arr[i] % 3 ==0:
         sum_val += arr[i]
 
 print(sum_val)
