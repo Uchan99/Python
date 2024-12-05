ef7db43c793a6fcd7b5d853df542c4c37480a59c
Hur Youchan <126740133+graphicst@users.noreply.github.com>
2024-12-06 03:31:16 +0900

    Title: 특이한 출력, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 241206/특이한 출력/special-print.py
+++ 241206/특이한 출력/special-print.py
@@ -2,7 +2,7 @@ n = int(input())
 cnt = 3
 for i in range(1, n+1):
     for j in range(1, n+1):
-        if j == cnt:
+        if (i + j) % 4 == 0:
             print(f"({i}, {j})", end='\n')
             cnt -= 1
         else:
