de9779e8000210a67e1d3d60db28ccecc9c6aaf2
Hur Youchan <126740133+graphicst@users.noreply.github.com>
2024-12-13 18:02:09 +0900

    Title: 배열 놀이, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241213/배열 놀이/play-with-array.py
@@ -0,0 +1,20 @@
+n, q = map(int, input().split())
+
+arr = list(map(int, input().split()))
+
+for i in range(q):
+    que = list(map(int, input().split()))
+    if que[0] == 1:
+        print(arr[que[1]-1])
+    elif que[0] == 2:
+        for j in range(n):
+            idx = -1
+            if arr[j] == que[1]:
+                idx = j +1
+                print(idx)
+                break
+        if idx == -1:
+            print(0)
+    elif que[0] == 3:
+        for j in range(que[1], que[2] + 1):
+            print(arr[j-1], end=' ')
