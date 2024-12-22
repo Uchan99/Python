172d582b64c61abb83759f3058a63fb266c24ec7
Hur Youchan <126740133+graphicst@users.noreply.github.com>
2024-12-22 23:16:49 +0900

    Title: 문자열 범위 출력하기 2, Time: 73ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241222/문자열 범위 출력하기 2/print-string-in-range-2.py
+++ 241222/문자열 범위 출력하기 2/print-string-in-range-2.py
@@ -2,8 +2,7 @@ arr = input()
 n = int(input())
 
 if len(arr) > n:
-    arr = arr[::-1]
-    for i in range(n):
+    for i in range(len(arr)- 1,len(arr) - 1 - n, -1):
         print(arr[i], end='')
 else:
     for i in range(n):
