d40eb7165aabe0d77216e442d31a328a13560350
Hur Youchan <126740133+graphicst@users.noreply.github.com>
2024-12-22 23:34:11 +0900

    Title: 문자열 범위 출력하기 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241222/문자열 범위 출력하기 2/print-string-in-range-2.py
+++ 241222/문자열 범위 출력하기 2/print-string-in-range-2.py
@@ -1,9 +1,12 @@
 arr = input()
 n = int(input())
 len_arr = len(arr)
+
 if len_arr > n:
-    for i in range(len_arr- 1,len_arr - 1 - n, -1):
+    # 문자열의 마지막 n개를 역순으로 출력
+    for i in range(len_arr - 1, len_arr - 1 - n, -1):
         print(arr[i], end='')
 else:
+    # 문자열을 반복해서 n개의 문자 출력
     for i in range(n):
-        print(arr[i], end='')
+        print(arr[i % len_arr], end='')
