240a927502fb314afbd2a95ac375c68a833fe9e6
Hur Youchan <126740133+graphicst@users.noreply.github.com>
2024-12-23 19:48:10 +0900

    Title: Run Length 인코딩, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241223/Run Length 인코딩/run-length-encoding.py
+++ 241223/Run Length 인코딩/run-length-encoding.py
@@ -9,22 +9,20 @@ if len(A) != 1:
             if A[i] == current:
                 cnt += 1
                 arr.append(current)
-                arr.append(str(cnt))
+                arr.append(cnt)
             else:
                 arr.append(current)
-                arr.append(str(cnt))
+                arr.append(cnt)
                 current = A[i]
                 cnt = 1
                 arr.append(current)
-                arr.append(str(cnt))
+                arr.append(cnt)
         else:
             if A[i] == current:
                 cnt += 1
             else:
                 arr.append(current)
-                # cnt를 문자열로 변환 후, 각 자릿수를 분리해서 추가
-                for digit in str(cnt):
-                    arr.append(digit)
+                arr.append(cnt)
                 current = A[i]
                 cnt = 1
 else:
