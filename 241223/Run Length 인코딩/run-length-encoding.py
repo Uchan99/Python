33de3d18c6d03e4c0c9b86bd8134076bc9db6abe
Hur Youchan <126740133+graphicst@users.noreply.github.com>
2024-12-23 19:07:45 +0900

    Title: Run Length 인코딩, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241223/Run Length 인코딩/run-length-encoding.py
+++ 241223/Run Length 인코딩/run-length-encoding.py
@@ -3,27 +3,31 @@ A = input()
 cnt = 1
 current = A[0]
 arr = []
-for i in range(1, len(A)):
-    if i == len(A) - 1:
-        if A[i] == current:
-            cnt += 1
-            arr.append(current)
-            arr.append(str(cnt))
+if len(A) != 1:
+    for i in range(1, len(A)):
+        if i == len(A) - 1:
+            if A[i] == current:
+                cnt += 1
+                arr.append(current)
+                arr.append(str(cnt))
+            else:
+                arr.append(current)
+                arr.append(str(cnt))
+                current = A[i]
+                cnt = 1
+                arr.append(current)
+                arr.append(str(cnt))
         else:
-            arr.append(current)
-            arr.append(str(cnt))
-            current = A[i]
-            cnt = 1
-            arr.append(current)
-            arr.append(str(cnt))
-    else:
-        if A[i] == current:
-            cnt += 1
-        else:
-            arr.append(current)
-            arr.append(str(cnt))
-            current = A[i]
-            cnt = 1
+            if A[i] == current:
+                cnt += 1
+            else:
+                arr.append(current)
+                arr.append(str(cnt))
+                current = A[i]
+                cnt = 1
+else:
+    arr.append(current)
+    arr.append(cnt)
 
 print(len(arr))
 for elem in arr:
