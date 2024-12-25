a06126a83e200499e5ac771dc179730dbb653020
Hur Youchan <126740133+graphicst@users.noreply.github.com>
2024-12-25 22:55:47 +0900

    Title: 부분문자열 위치 구하기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241225/부분문자열 위치 구하기/find-location-of-substring.py
@@ -0,0 +1,28 @@
+a = input()
+b = input()
+
+idx = -1
+
+find = False
+for i in range(len(a) - len(b)):
+    if find == True:
+        break
+    else:
+        if a[i] == b[0]:
+            idx = i
+            for j in range(1, len(b)):
+                if j == len(b) - 1:
+                    find = True
+                if a[i + j] == b[j]:
+                    continue
+                else:
+                    idx = -1
+                    break
+
+print(idx)
+    
+    
+            
+            
+
+        
