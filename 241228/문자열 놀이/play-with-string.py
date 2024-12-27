10ce45bbb45eab7a3c243091a221d6f0882acff1
Hur Youchan <126740133+graphicst@users.noreply.github.com>
2024-12-28 01:16:14 +0900

    Title: 문자열 놀이, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 241228/문자열 놀이/play-with-string.py
+++ 241228/문자열 놀이/play-with-string.py
@@ -6,9 +6,13 @@ for _ in range(q):
     t, a, b = input().split()
     t = int(t)
     if t == 1:
+        s = list(s)
         a = int(a)
         b = int(b)
-        s = s[:a-1] + s[b-1] + s[a:b-1] + s[a-1] + s[b:]
+        tmp = s[a-1]
+        s[a-1] = s[b-1]
+        s[b-1] = tmp
+        s = ''.join(s)
         print(s)
     elif t == 2:
         s  = list(s)
